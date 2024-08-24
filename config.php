475dc66e06ce12cafd2dbfbc5b497b13ada260f5
abdulnasir25 <abdulnasirshah25@gmail.com>
2024-08-25 03:27:39 +0500

    Inital setup

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'https://www.it-doneright.co.uk/shop/newshop/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://www.it-doneright.co.uk/shop/newshop/');
+
+// DIR
+define('DIR_APPLICATION', '/home/itdoneri/public_html/shop/newshop/catalog/');
+define('DIR_SYSTEM', '/home/itdoneri/public_html/shop/newshop/system/');
+define('DIR_IMAGE', '/home/itdoneri/public_html/shop/newshop/image/');
+define('DIR_STORAGE', '/home/itdoneri/public_html/shop/newshop/storage/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'itdoneri_itdoneri_nasir24');
+define('DB_PASSWORD', 'Z58L&jT%a&X@ZyZg');
+define('DB_DATABASE', 'itdoneri_newshop_nasiroc3');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'itd_');
